0558ef9fbb30c72e24a7c3e3ff332ed82c68f450
Emeka <okocha007@gmail.com>
2021-11-26 02:10:46 +0100

    our initial commit

---
+++ okoch.js
@@ -0,0 +1,40 @@
+let tt = function(){
+    let datte = new Date();
+    let t= document.getElementById("timerP");
+    t.innerHTML="Today is: " + datte;
+    var ourTime;
+    alert("you have been updated with the most current date as" + datte)
+    
+    }
+
+let calculateBmi = function(){
+let name= document.getElementById("bminame").value;
+let mail= document.getElementById("bmimail").value;
+//let age= getElementById("bmiage");
+if (name ==="" || mail ===""){
+    alert("kindly fill all required fields");
+    name="";
+}
+else{
+    alert("mr "+ name +" "+"your body mass index is");
+    }
+}
+let signUpp = function(){
+        let fn= document.getElementById("fnm").value;
+        let un= document.getElementById("unm").value;
+        let pw= document.getElementById("pwd").value;
+   // let em= document.getElementById("em").value;
+    //let age= getElementById("bmiage");
+    if (fn ==="" || un ==="" || pw ===""){
+        alert("kindly fill all required fields");
+   //    name="";
+    }
+    else{
+        alert("mr "+ fn +" "+"your account has been created successfully");
+        }
+    }
+
+let CancelB =function(){
+    
+
+}
