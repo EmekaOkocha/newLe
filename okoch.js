74e6afdb104456cab82d99f66f98b0bd4f2fe68a
Chukwuemeka Okocha <okocha007@gmail.com>
2021-12-01 10:39:31 +0100

    aside bar working with sign in page

--- okoch.js
+++ okoch.js
@@ -1,13 +1,80 @@
+let move =5;
+let intt;
+let changge= false;
+function time(){
+    let date = new Date();
+    let hr = date.getHours();
+    let mn = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let currentTime =hr +":"+ mn +":"+ sec;
+    let h3 = document.getElementById("timerP");
+    let tt = setTimeout(function(){time()}, 1000);
+    h3.innerHTML= currentTime;
+}
+time();
+function scrollImage(){
+    let srcImg = document.getElementById("d2");
+    //srcImg.style.position = "relative";
+    //srcImg.style.left = (move + 5)+"px";
+    //intt = setInterval(scrollImage,5);
+}
+//scrollImage();
+function rectt(){
+    let can = getElementById("canntt");
+  //  can.innerHTML="our Canvas starts here."
+    let renCan = can.getContext("2D");
+    renCan.fillStyle= "rgb(255,0,0)";
+    renCan.fillRect(20,20,80,40);
+//  renCan.strokeRect(30,50.120,100);
+}
+var imgObj = null;
+function init(){
+ imgObj = document.getElementById("cann");
+ imgObj.style.position= 'relative'; 
+ imgObj.style.left = '0px'; 
+}
+function moveRight(){
+ imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+
+}
+window.onload = init;
+
+
+let userData={
+    "Username":"emeka",
+    "password":"mark"
+}
+let username;
+let password;
+
 let tt = function(){
     let datte = new Date();
     let t= document.getElementById("timerP");
     t.innerHTML="Today is: " + datte;
     var ourTime;
-    alert("you have been updated with the most current date as" + datte)
+    alert("you have been updated with the most current date as " + datte)
     
     }
-
-let calculateBmi = function(){
+function signedIn(){
+  //  const fs = require("fs");
+    let username = document.getElementById("fnm");
+    let password = document.getElementById("unm");
+    let a = username.value;
+    let b = password.value;
+    let jsonFile;
+    let stringifyFile;
+    if(a ==="" || b ===""){
+        alert("invalid username or password");
+    }
+    else{
+  /*      jsonFile = [{b:a}];
+        stringifyFile = JSON.stringify(jsonFile);
+        fs.appendfile */
+        alert("You are now logged in as " + a);
+    }
+}
+/*let calculateBmi = function(){
 let name= document.getElementById("bminame").value;
 let mail= document.getElementById("bmimail").value;
 //let age= getElementById("bmiage");
@@ -17,8 +84,9 @@ if (name ==="" || mail ===""){
 }
 else{
     alert("mr "+ name +" "+"your body mass index is");
-    }
-}
+    } 
+
+}*/
 let signUpp = function(){
         let fn= document.getElementById("fnm").value;
         let un= document.getElementById("unm").value;
@@ -26,15 +94,27 @@ let signUpp = function(){
    // let em= document.getElementById("em").value;
     //let age= getElementById("bmiage");
     if (fn ==="" || un ==="" || pw ===""){
-        alert("kindly fill all required fields");
+        alert("Oga kindly fill all required fields");
    //    name="";
     }
     else{
         alert("mr "+ fn +" "+"your account has been created successfully");
         }
+    
     }
-
-let CancelB =function(){
-console.log("hello world")
+    
+function sideM(){
+    let menu = document.getElementById("aa");
+    let men = document.getElementById("as");
+    if(changge === false){
+        men.style.width="150px";
+        menu.style.visibility= "visible";
+            changge= true;
+    }
+    else if(changge === true){
+        men.style.width= "50px";
+        menu.style.visibility= "hidden";
+        changge= false;
+    }
     
 }
--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 const http = require("http");
 const fille = require("fs");
+const url = require("url");
+//const server = http.createServer;
+//server.listen=3000;
+//res.writeHead(200, { 'Content-Type': 'text/html' });
+//console.log("listening...")
+
+function readF(){
+let ff = fille.readFile("names.json",(error)=>{
+    if (error){
+        console.log(error);
+    }
+});
+}
+let apndData = function(){
+let parsIt = JSON.parse(ff);
+let writF = fille.appendFile("names.json", "Mark : 24",(err)=>{
+    if(err){
+    console.log("error")
+    alert("oops! our first error");}
+        else{alert("file written succesfully")}
+});
+}
+
+/*function onRequest(request, response){
+    response.writeHead(200,{'content-Type': text/plain});
+}
 const server = http.createServer;
 server.listen=3000;
 console.log("listening...")
+fille.readFile("names.json", (err,data)=>{
+
+}); */
