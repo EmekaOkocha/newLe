0a6b4c40076427f2b1ed91677dc78a9072938f4f
Chukwuemeka Okocha <okocha007@gmail.com>
2021-12-12 00:14:45 +0100

    modified server.js and names.json  files

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const http = require("http");
+var fille = require("fs");
+/*const http = require("http");
 const fille = require("fs");
-const url = require("url");
+const url = require("url");*/
 //const server = http.createServer;
 //server.listen=3000;
 //res.writeHead(200, { 'Content-Type': 'text/html' });
 //console.log("listening...")
 
 function readF(){
-/* let ff = fille.readFile("names.json",(error)=>{
+ var ff = fille.readFile("names.json",(error)=>{
     if (error){
         alert("file not found. kindly check the file name or contact your administrator.")
         console.log(error);
     }
-    alert S("ff");
-});*/
-alertS("ff");
-}
+    console.log(ff);
+    });
+};
 let apndData = function(){
-let parsIt = JSON.parse(ff);
+//let parsIt = JSON.parse(ff);
 let writF = fille.appendFile("names.json", "Mark : 24",(err)=>{
     if(err){
     console.log("error")
-    alert("oops! our first error");}
-        else{alert("file written succesfully");
+    //alert("oops! our first error");
     }
+    else{alert("file written succesfully");
+        }
 });
 }
 
